b5e863765e904c5cd41cafa9ab276842f06c7df9
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-11-14 18:04:19 +0000

    chore: reorg again

--- looping_for_resource_blocks/dynamic_n_instances/main.tf
+++ looping_for_resource_blocks/dynamic_n_instances/main.tf
@@ -2,6 +2,7 @@ provider "azurerm" {
   features {}
 }
 
+# These resources ensure we deploy to a random region and use a random suffix for our resource names
 module "regions" {
   source  = "Azure/regions/azurerm"
   version = ">= 0.4.0"
@@ -22,24 +23,39 @@ module "naming" {
   suffix  = [random_pet.example.id]
 }
 
+# This is the interesting part of the example
 resource "azurerm_resource_group" "example" {
   name     = module.naming.resource_group.name
   location = module.regions.regions[random_integer.region_index.result].name
 }
 
 locals {
-  subnets = toset([{
-    name           = "subnet1"
-    address_prefix = cidrsubnet(module.naming.virtual_network.address_space[0], 8, 0)
-  }])
+  address_prefix = "192.168.0.0/16"
+  subnets = toset([
+    {
+      name           = "subnet1"
+      address_prefix = cidrsubnet(local.address_prefix, 8, 0)
+    },
+    {
+      name           = "subnet2"
+      address_prefix = cidrsubnet(local.address_prefix, 8, 1)
+    },
+  ])
 }
-
 
 resource "azurerm_virtual_network" "example" {
   name                = module.naming.virtual_network.name
-  address_space       = ["192.168.0.0/24"]
+  address_space       = [local.address_prefix]
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
-
+  # Here we use a set of objects to create multiple subnets.
+  # For dynamics we don't needs to use a map, or a set of strings like we do for resources & modules.
+  dynamic "subnet" {
+    for_each = local.subnets
+    content {
+      name           = subnet.value.name
+      address_prefix = subnet.value.address_prefix
+    }
+  }
 }
---
+++ looping_for_resource_blocks/dynamic_singleton/main.tf
@@ -0,0 +1,3 @@
+resource "terraform_" "name" {
+
+}
