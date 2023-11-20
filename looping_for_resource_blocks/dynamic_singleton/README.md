230a48298b80f6ce2bc5d93ceaedc5b600a46398
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-11-20 16:52:52 +0000

    feat: create example

---
+++ looping_for_resource_blocks/dynamic_singleton/README.md
@@ -0,0 +1,8 @@
+# Dynamic singleton
+
+- [back home](../../)
+- [back to parent](../)
+
+This pattern is used to create zero or one blocks within a resource, based on a variable or other condition.
+
+> Note this example does not create a valid plan. It is intended to show the pattern only.
--- looping_for_resource_blocks/dynamic_singleton/main.tf
+++ looping_for_resource_blocks/dynamic_singleton/main.tf
@@ -1,3 +1,19 @@
-resource "terraform_data" "name" {
+locals {
+  thing_enabled = true
+}
 
+resource "my_resource" "example" {
+  input = "foo"
+
+  # Here we construct a dynamic block where there can be either zero or one instances.
+  # We do this by using a `for_each` expression that returns either an empty set or a set with one element.
+  # In this scenario it is common to not use the `my_block.value` expression at all, and instead use
+  # references to other values.
+  dynamic "my_block" {
+    for_each = local.lifecycle_enabled ? toset([1]) : toset([])
+
+    content {
+      replace_triggered_by = [""]
+    }
+  }
 }
